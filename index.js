90575f8661fba499540cc7d2384832f3363bceb4
JoshGrant5 <joshgg@icloud.com>
2020-09-25 19:32:22 +0000

    Bank account using OOP

--- index.js
+++ index.js
@@ -1,29 +1,47 @@
-let balance = 500.00;
+/*
+* Allow multiple accounts to be created
+* Each account can have many transactions
+* Allow withdrawals and deposits into accounts
+* Allow us to retrieve the transaction history of an account (all withdrawals and deposits)
+* Allow us to retrieve the current balance of the account at any time
+* Don't allow withdrawals that exceed the remaining balance of the account
+*/
 
-class Withdrawal {
+class Account {
 
-  constructor(amount) {
-    this.amount = amount;
+  constructor(name, balance) {
+    this.name = name;
+    this.balance = balance;
+    this.history = [];
   }
 
-  commit() {
-    balance -= this.amount;
+  withdrawal(amount) {
+    if (amount < this.balance) {
+      console.log(`${amount} withdrawn from account: ${this.name}`);
+      this.balance -= amount;
+      console.log(`New balance: ${this.balance}`);
+      this.history.push(`Withdrawl: ${amount}`);
+    } else {
+      console.log('Error: Insufficient funds');
+    }
+  }
+
+  deposit(amount) {
+    this.balance += amount;
+    console.log(`${amount} deposited into account: ${this.name}`);
+    console.log(`New balance: ${this.balance}`);
+    this.history.push(`Deposit: ${amount}`);
   }
 
 }
 
-
-
-
 // DRIVER CODE BELOW
 // We use the code below to "drive" the application logic above and make sure it's working as expected
 
-t1 = new Withdrawal(50.25);
-t1.commit();
-console.log('Transaction 1:', t1);
-
-t2 = new Withdrawal(9.99);
-t2.commit();
-console.log('Transaction 2:', t2);
-
-console.log('Balance:', balance);
+let account1 = new Account('Chequing', 1000);
+console.log(account1);
+account1.withdrawal(200);
+account1.deposit(50);
+console.log(account1.history);
+console.log(account1.balance);
+account1.withdrawal(10000);
